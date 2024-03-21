985ea7463de80daa54bb2a721ebb7def24b077c3
Jan Niewielski <39317379+jniewiel@users.noreply.github.com>
2024-03-21 20:35:00 +0000

    Made changes for errors due to user input or nonexistent track

--- app.rb
+++ app.rb
@@ -35,46 +35,63 @@ get("/search_results") do
     erb(:search)
   else
     results_corrected = parsed_data_correct["corrections"]["correction"]["track"]
-    @corrected_artist = results_corrected["artist"]["name"].gsub(" ", "%20")
-    @corrected_song = results_corrected["name"].gsub(" ", "%20")
+    @corrected_artist = results_corrected["artist"]["name"]
+    @corrected_song = results_corrected["name"]
 
     api_info = build_api_url("track.getinfo", @corrected_artist, @corrected_song)
     raw_data_info = HTTP.get(api_info).to_s
     parsed_data_info = JSON.parse(raw_data_info)
 
-    if parsed_data_info["track"].nil? || parsed_data_info["track"]["wiki"].nil?
+    if parsed_data_info["track"].nil?
       erb(:search)
     else
       results_info = parsed_data_info["track"]
-      @summary = results_info["wiki"]["summary"]
-      @cover = results_info["album"] && results_info["album"]["image"][2]["#text"]
-
-      api_search = build_api_url("track.getsimilar", @corrected_artist, @corrected_song) + "&limit=5"
-      final_recommendations = []
-
-      raw_data = HTTP.get(api_search).to_s
-      parsed_data = JSON.parse(raw_data)
-
-      if parsed_data && parsed_data["similartracks"]["track"].any?
-        5.times do |index|
-          results = parsed_data["similartracks"]["track"]
-          @rec_artist_name = results[index]["artist"]["name"]
-          @rec_song_name = results[index]["name"]
-          @rec_track_link = "https://www.last.fm/music/#{@rec_artist_name}/_/#{@rec_song_name}"
-    
-          track = { @rec_artist_name => @rec_song_name }
-          bundle = { track => @rec_track_link }
-    
-          final_recommendations.push(bundle)
-        end
-      end
-
-      @final = final_recommendations
-      erb(:search_results)
     end
+  
+    if results_info == nil
+      erb(:search)
+    elsif results_info["wiki"] == nil
+      erb(:search)
+    else
+      @summary = results_info["wiki"]["summary"]
+    end
+  
+    if results_info == nil
+      erb(:search)
+    elsif results_info["album"] == nil
+      @cover = nil
+    else
+      @cover = results_info["album"]["image"][2]["#text"]
+    end
+
+    # -----------------------------
+    api_search = build_api_url("track.getsimilar", @corrected_artist, @corrected_song) + "&limit=5"
+    final_recommendations = []
+
+    raw_data = HTTP.get(api_search).to_s
+    @parsed_data = JSON.parse(raw_data)
+
+    if @parsed_data.nil? || @parsed_data == {} || @parsed_data["similartracks"]["track"] == []
+      erb(:search)
+    else
+      5.times do |index|
+        results = @parsed_data["similartracks"]["track"]
+
+        @rec_artist_name = results[index]["artist"]["name"]
+        @rec_song_name = results[index]["name"]
+        @rec_track_link = "https://www.last.fm/music/#{@rec_artist_name}/_/#{@rec_song_name}"
+    
+        track = { @rec_artist_name => @rec_song_name }
+        bundle = { track => @rec_track_link }
+    
+        final_recommendations.push(bundle)
+      end
+    end
+
+    @final = final_recommendations
+    erb(:search_results)
   end
 end
-
 
 
 # ---------------------------------------------------------- #
